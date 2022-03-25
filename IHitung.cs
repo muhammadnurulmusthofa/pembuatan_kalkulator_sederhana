6047378bc8993ca327ea1ffe9e82b764107368f2
muhammad Nurul Musthofa <99570369+muhammadnurulmusthofa@users.noreply.github.com>
2022-03-25 09:38:51 +0700

    Create IHitung.cs
    interface IHitung.C#

---
+++ IHitung.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_pbo_2
+{
+    interface IHitung
+    {
+        double Tambah(double a, double b);
+        double Kurang (double a, double b);
+
+    }
+
+}
